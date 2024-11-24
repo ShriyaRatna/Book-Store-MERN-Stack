b54b3ded2de1ac7b366866fad2d59392039a407a
ShriyaRatna <shriya.ratna7@gmail.com>
2024-11-24 12:20:50 +0530

    Finished Backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+// import Home from './pages/Home'
+// import Home from './pages/Home'
+// import Home from './pages/Home'
+// import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <Routes>
+      {/* <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} /> */}
+    </Routes>
+  );
+};
+
+export default App;
